0e618ffa69644e4e65e7adb12e4e678d0beb1803
Andrew Virts <asvirts@outlook.com>
2024-08-17 19:57:42 -0500

    reset

--- Jabba.App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Jabba.App.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
